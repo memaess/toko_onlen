075a9cf9f998363d2753da8a694ff51d0bfdaa46
Me Gilang R <megilangr1@gmail.com>
2020-04-15 02:20:20 +0700

    Seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -11,6 +12,32 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user = User::firstOrCreate([
+            'name' => 'Administrator 01',
+            'email' => 'admin@mail.com',
+            'password' => bcrypt('nanozero1')
+        ]);
+        $user->assignRole('Administrator');
+
+        $user = User::firstOrCreate([
+            'name' => 'Employee 01',
+            'email' => 'employee@mail.com',
+            'password' => bcrypt('nanozero1')
+        ]);
+        $user->assignRole('Employee');
+
+        $user = User::firstOrCreate([
+            'name' => 'Owner 01',
+            'email' => 'owner@mail.com',
+            'password' => bcrypt('nanozero1')
+        ]);
+        $user->assignRole('Owner');
+
+        $user = User::firstOrCreate([
+            'name' => 'Buyer 01',
+            'email' => 'buyer@mail.com',
+            'password' => bcrypt('nanozero1')
+        ]);
+        $user->assignRole('Buyer');
     }
 }
